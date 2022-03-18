0cb2f4ac690cd31b560b47a03ad3d9268b46ca2e
disooloo <disooloo@mail.ru>
2022-03-18 18:29:38 +0500

    login

---
+++ HR Department/HR Department/views/layouts/LoginWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HR_Department.views.layouts
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void dragme(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch (Exception)
+            {
+
+                //throw;
+            }
+        }
+
+        private void CloseLogin(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
